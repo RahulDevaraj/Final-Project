9e3615f2fddc568df698851838d86327042c7ba2
RahulDevaraj <kannan.vegeta@gmail.com>
2023-02-15 23:15:10 -0800

    User Order Page Started

--- frontend/src/pages/CartPage.js
+++ frontend/src/pages/CartPage.js
@@ -1,5 +1,37 @@
+import { Row, Col, Container, Alert, ListGroup, Button } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import CartItemComponent from "../components/CartItemComponent";
 const CartPage = () => {
-    return ( <p>CartPage</p> );
-}
- 
+  return (
+    <Container fluid>
+      <Row className="mt-4">
+        <Col md={8}>
+          <h1>Shopping Cart</h1>
+          <ListGroup variant="flush">
+            {Array.from({ length: 3 }).map((item, idx) => (
+              <CartItemComponent key={idx} />
+            ))}
+          </ListGroup>
+          <Alert variant="info">Cart is empty</Alert>
+        </Col>
+        <Col md={4}>
+          <ListGroup>
+            <ListGroup.Item>
+              <h3>Subtotal</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              Price<span className="fw-bold">$672</span>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <LinkContainer to="/user/order-details">
+                <Button type="button">CheckOut</Button>
+              </LinkContainer>
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
 export default CartPage;
--- frontend/src/pages/user/UserOrdersPage.js
+++ frontend/src/pages/user/UserOrdersPage.js
@@ -1,5 +1,41 @@
+import { Row, Col, Table } from "react-bootstrap";
 const UserOrdersPage = () => {
-    return ( <p>UserOrdersPage</p> );
-}
- 
+  return (
+    <Row className="m-5">
+      <Col md={12}>
+        <h1>My orders</h1>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Username</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td colSpan={2}>Larry the Bird</td>
+              <td>@twitter</td>
+            </tr>
+          </tbody>
+        </Table>
+      </Col>
+    </Row>
+  );
+};
+
 export default UserOrdersPage;
