6190ae004d9b5ee1546a0df9e306915f6cc45113
RahulDevaraj <kannan.vegeta@gmail.com>
2023-02-20 11:56:33 -0800

    Backend initial setup

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const apiRoutes = require("./routes/apiRoutes");
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello World!" });
+});
+app.get("/a", (req, res, next) => {
+  setTimeout(() => {
+    try {
+      aconsole.log("asynchronouse code");
+    } catch (er) {
+      next(er);
+    }
+  }, 1000);
+  // next(new Error("some error occured"));
+});
+
+app.use((error, req, res, next) => {
+  console.error(error);
+  next(error);
+});
+app.use((error, req, res, next) => {
+  res.status(500).json({
+    message: error.message,
+    stack: error.stack,
+  });
+});
+
+app.use("/api", apiRoutes);
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
