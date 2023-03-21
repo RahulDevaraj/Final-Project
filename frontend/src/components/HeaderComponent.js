911c301728d322a476453af9f073e81b4ae544e5
RahulDevaraj <kannan.vegeta@gmail.com>
2023-03-20 21:25:49 -0700

    Integrating Login

--- frontend/src/components/HeaderComponent.js
+++ frontend/src/components/HeaderComponent.js
@@ -1,35 +1,41 @@
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
-import Badge from "react-bootstrap/Badge";
-import Form from "react-bootstrap/Form";
-import Dropdown from "react-bootstrap/Dropdown";
-import DropdownButton from "react-bootstrap/DropdownButton";
-import Button from "react-bootstrap/Button";
-import { InputGroup } from "react-bootstrap";
+import {
+  Navbar,
+  Nav,
+  Container,
+  NavDropdown,
+  Badge,
+  Form,
+  DropdownButton,
+  Dropdown,
+  Button,
+  InputGroup,
+} from "react-bootstrap";
+
 import { LinkContainer } from "react-router-bootstrap";
 import { Link } from "react-router-dom";
+import { logout } from "../redux/actions/userActions";
+import { useDispatch } from "react-redux";
 
 const HeaderComponent = () => {
+    const dispatch = useDispatch();
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
         <LinkContainer to="/">
-          <Navbar.Brand href="/">KartAid</Navbar.Brand>
+          <Navbar.Brand href="/">BEST ONLINE SHOP</Navbar.Brand>
         </LinkContainer>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
             <InputGroup>
-              <DropdownButton id="dropdown-basic-button" title="All Categories">
+              <DropdownButton id="dropdown-basic-button" title="All">
                 <Dropdown.Item>Electronics</Dropdown.Item>
-                <Dropdown.Item>Books</Dropdown.Item>
                 <Dropdown.Item>Cars</Dropdown.Item>
+                <Dropdown.Item>Books</Dropdown.Item>
               </DropdownButton>
-              <Form.Control type="text" placeholder="Search" />
+              <Form.Control type="text" placeholder="Search in shop ..." />
               <Button variant="warning">
-                <i className="bi bi-search"></i>
+                <i className="bi bi-search text-dark"></i>
               </Button>
             </InputGroup>
           </Nav>
@@ -47,14 +53,13 @@ const HeaderComponent = () => {
                 as={Link}
                 to="/user/my-orders"
               >
-                My Orders
+                My orders
               </NavDropdown.Item>
               <NavDropdown.Item eventKey="/user" as={Link} to="/user">
-                My Profile
+                My profile
               </NavDropdown.Item>
-              <NavDropdown.Item>Logout</NavDropdown.Item>
+              <NavDropdown.Item onClick={() => dispatch(logout())}>Logout</NavDropdown.Item>
             </NavDropdown>
-
             <LinkContainer to="/login">
               <Nav.Link>Login</Nav.Link>
             </LinkContainer>
@@ -63,12 +68,11 @@ const HeaderComponent = () => {
             </LinkContainer>
             <LinkContainer to="/cart">
               <Nav.Link>
-                {" "}
                 <Badge pill bg="danger">
                   2
                 </Badge>
-                <i className="bi bi-cart"></i>
-                <span className="ms-l">CART</span>
+                <i className="bi bi-cart-dash"></i>
+                <span className="ms-1">CART</span>
               </Nav.Link>
             </LinkContainer>
           </Nav>
@@ -79,3 +83,4 @@ const HeaderComponent = () => {
 };
 
 export default HeaderComponent;
+
