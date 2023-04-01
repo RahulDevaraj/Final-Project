df58cbdfe1b827ea013257c39b6634adf2d7ce8d
RahulDevaraj <kannan.vegeta@gmail.com>
2023-04-01 15:01:09 -0700

    Working on Edit Product Page

--- frontend/src/pages/user/UserOrdersPage.js
+++ frontend/src/pages/user/UserOrdersPage.js
@@ -1,43 +1,14 @@
-import { Row, Col, Table } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import UserOrdersPageComponent from "./components/UserOrdersPageComponent";
+import axios from "axios";
+
+const getOrders = async () => {
+    const { data } = await axios.get("/api/orders");
+    return data;
+}
+
 const UserOrdersPage = () => {
-  return (
-    <Row className="m-5">
-      <Col md={12}>
-        <h1>My orders</h1>
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>User</th>
-              <th>Date</th>
-              <th>Total</th>
-              <th>Delivered</th>
-              <th>Order Details</th>
-            </tr>
-          </thead>
-          <tbody>
-            {["bi bi-check-lg text-success", "bi bi-x-lg text-danger"].map(
-              (item, index) => (
-                <tr key={index}>
-                  <td>{index + 1}</td>
-                  <td>Mark Tw</td>
-                  <td>2022-08-12</td>
-                  <td>@$123</td>
-                  <td>
-                    <i className={item}></i>
-                  </td>
-                  <td>
-                    <Link to="/user/order-details">Order</Link>
-                  </td>
-                </tr>
-              )
-            )}
-          </tbody>
-        </Table>
-      </Col>
-    </Row>
-  );
+  return <UserOrdersPageComponent getOrders={getOrders} />;
 };
 
 export default UserOrdersPage;
+
